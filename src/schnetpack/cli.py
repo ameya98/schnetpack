171f4fded31e623747cf1696656c2151cfa425ba
Ameya Daigavane <ameya.d.98@gmail.com>
2023-03-10 19:09:57 -0500

    Try some more to avoid checkpointing.

--- src/schnetpack/cli.py
+++ src/schnetpack/cli.py
@@ -158,7 +158,7 @@ def train(config: DictConfig):
 
     # Train the model
     log.info("Starting training.")
-    trainer.fit(model=task, datamodule=datamodule, ckpt_path=config.run.ckpt_path)
+    trainer.fit(model=task, datamodule=datamodule, ckpt_path=None)
 
     # Evaluate model on test set after training
     log.info("Starting testing.")
