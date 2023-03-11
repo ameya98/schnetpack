7c8cb29bf883b3c249dab5ffc4a00b87406e1cc1
Ameya Daigavane <ameya.d.98@gmail.com>
2023-03-11 12:41:37 -0500

    Try not compiling model.

--- src/schnetpack/cli.py
+++ src/schnetpack/cli.py
@@ -108,6 +108,7 @@ def train(config: DictConfig):
     log.info(f"Instantiating model <{config.model._target_}>")
     print(config.model)
     model = hydra.utils.instantiate(config.model)
+    print("model", model)
     # model = e3nn.util.jit.compile(model)
     #model = hydra.utils.instantiate(config.model)
 
